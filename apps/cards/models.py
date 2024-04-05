7ba4771610e8c043ca17ea910446a179cf2772f3
WiderAI <mukhsinmukhtorov02@gmail.com>
2024-04-05 13:35:04 +0500

    first commit

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from apps.decks.models import Deck
 
 # Create your models here.
 
 class Card(models.Model):
     question = models.CharField(max_length=255)
     answer = models.TextField()
+
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.question
